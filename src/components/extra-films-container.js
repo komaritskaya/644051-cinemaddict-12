f84eaf306a20c5da5b8a772c31bcc8a3829ae8cc
Nadezhda Komaritckaia <komaritskaya@Nadezhdas-MacBook-Pro.local>
2020-08-20 03:06:41 +0300

    Разбивка кода на модули

---
+++ src/components/extra-films-container.js
@@ -0,0 +1,6 @@
+export const createExtraFilmsContainerTemplate = (listTitle) => (
+  `<section class="films-list--extra">
+    <h2 class="films-list__title">${listTitle}</h2>
+    <div class="films-list__container"></div>
+  </section>`
+);
---
+++ src/components/films-section.js
@@ -0,0 +1,3 @@
+export const createFilmsSectionTemplate = () => (
+  `<section class="films"></section>`
+);
